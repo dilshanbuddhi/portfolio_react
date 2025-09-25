fcfb659c1ff0f2a66554d51626cac6980c7e49f1
dilshanbuddhi <dilshanbuddhi40@gmail.com>
2025-09-25 10:22:46 +0530

    contact info

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -3,27 +3,37 @@ import emailIcon from "../../assets/email-icon.svg";
 import phoneIcon from "../../assets/phone-icon.svg"
 import { Form } from "../Form/Form";
 
+export function Contact() {
+    return (
+        <Container id="contact">
+            <header>
+                <h2>Contact</h2>
+                <p>Ready to get started on your project?</p>
+                <p>Contact me now for a Free consultation.</p>
+            </header>
 
-export function Contact(){
+            <div className="contacts">
+                <div>
+                    <strong>Address: </strong>
+                    <span>No. 91, Kumarakanda, Maggona, Kaluthara</span>
+                </div>
+                <div>
+                    <a href="mailto:dilshanbuddhi40@gmail.com">
+                        <img src={emailIcon} alt="Email" />
+                    </a>
+                    <a href="mailto:dilshanbuddhi40@gmail.com">
+                        dilshanbuddhi40@gmail.com
+                    </a>
+                </div>
+                <div>
+                    <a href="tel:+94782960721">
+                        <img src={phoneIcon} alt="Phone No" />
+                    </a>
+                    <a href="tel:+94782960721">(+94) 78 296 0721</a>
+                </div>
+            </div>
 
-  return(
-    <Container id="contact">
-      <header>
-        <h2>Contact</h2>
-        <p>Ready to get started on your project? </p>
-        <p>Contact me now for a Free consultation.</p>
-      </header>
-      <div className="contacts">
-        <div>
-        <a href="mailto:vinayak@vinayaksingh.com"><img src={emailIcon} alt="Email" /></a> 
-          <a href="mailto:vinayak@vinayaksingh.com">vinayak@vinayaksingh.com</a>
-        </div>
-        <div>
-        <a href="tel:+919630576848"><img src={phoneIcon} alt="Phone No" /></a>
-          <a href="tel:+919630576848">(+91) 9630576848</a>
-        </div>  
-      </div>
-      <Form></Form>
-    </Container>
-  )
+            <Form />
+        </Container>
+    );
 }
