0fa7556e8fb90c75667c05f7f80fc78a13a1da6d
dilshanbuddhi <dilshanbuddhi40@gmail.com>
2025-09-25 11:22:32 +0530

    react version updated

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -7,54 +7,55 @@ import telegram from '../../assets/telegram.svg'
 import instagramIcon from '../../assets/instagram.svg'
 
 export function Footer() {
-  return (
-      <Container className="footer">
-        <a href="#" className="logo">
-          <span>www.dilshan</span>
-          <span>buddhi.lk</span>
-        </a>
-        <div>
-          <p>
-            This Website was made with <img src={reactIcon} alt="React" />
-          </p>
-        </div>
-        <div className="social-media">
-          <a
-              href="https://www.linkedin.com/in/your-linkedin-username"
-              target="_blank"
-              rel="noreferrer"
-          >
-            <img src={linkedin} alt="Linkedin" />
-          </a>
-          <a
-              href="https://github.com/your-github-username"
-              target="_blank"
-              rel="noreferrer"
-          >
-            <img src={githubIcon} alt="GitHub" />
-          </a>
-          <a
-              href="https://api.whatsapp.com/send/?phone=%2B94782960721&text=Hello+Dilshan,+I+found+your+contact+through+portfolio+site."
-              target="_blank"
-              rel="noreferrer"
-          >
-            <img src={whatsapp} alt="Whatsapp" />
-          </a>
-          <a
-              href="https://t.me/your-telegram-username"
-              target="_blank"
-              rel="noreferrer"
-          >
-            <img src={telegram} alt="telegram" />
-          </a>
-          <a
-              href="https://www.instagram.com/your-instagram-username"
-              target="_blank"
-              rel="noreferrer"
-          >
-            <img src={instagramIcon} alt="Instagram" />
-          </a>
-        </div>
-      </Container>
-  )
+    return (
+        <Container className="footer">
+            <a href="/" className="logo">
+                <span>www.dilshan</span>
+                <span>buddhi.lk</span>
+            </a>
+
+            <div>
+                <p>
+                    This Website was made with <img src={reactIcon} alt="React" />
+                </p>
+            </div>
+            <div className="social-media">
+                <a
+                    href="https://www.linkedin.com/in/your-linkedin-username"
+                    target="_blank"
+                    rel="noreferrer"
+                >
+                    <img src={linkedin} alt="Linkedin" />
+                </a>
+                <a
+                    href="https://github.com/your-github-username"
+                    target="_blank"
+                    rel="noreferrer"
+                >
+                    <img src={githubIcon} alt="GitHub" />
+                </a>
+                <a
+                    href="https://api.whatsapp.com/send/?phone=%2B94782960721&text=Hello+Dilshan,+I+found+your+contact+through+portfolio+site."
+                    target="_blank"
+                    rel="noreferrer"
+                >
+                    <img src={whatsapp} alt="Whatsapp" />
+                </a>
+                <a
+                    href="https://t.me/your-telegram-username"
+                    target="_blank"
+                    rel="noreferrer"
+                >
+                    <img src={telegram} alt="telegram" />
+                </a>
+                <a
+                    href="https://www.instagram.com/your-instagram-username"
+                    target="_blank"
+                    rel="noreferrer"
+                >
+                    <img src={instagramIcon} alt="Instagram" />
+                </a>
+            </div>
+        </Container>
+    )
 }
--- src/components/Project/Project.tsx
+++ src/components/Project/Project.tsx
@@ -1,6 +1,5 @@
 import { Container } from "./styles";
 import githubIcon from "../../assets/github.svg";
-import externalLink from "../../assets/external-link.svg";
 
 export function Project() {
   return (
