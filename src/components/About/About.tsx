65858bbab6178ad4da0252b64a7ad593cabb2b3a
dilshanbuddhi <dilshanbuddhi40@gmail.com>
2025-09-25 10:04:08 +0530

    about info

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,17 +1,15 @@
 import { Container } from "./styles";
-import VinayakSingh from "../../assets/WhatsApp Image 2025-09-19 at 10.30.30_a39e2455.jpg";
-import python from "../../assets/python.svg"
-import java from "../../assets/java.svg"
-import wordpress from "../../assets/wordpress.svg";
-import shopify from "../../assets/shopify.svg";
+import myPhoto from "../../assets/WhatsApp Image 2025-09-19 at 10.30.30_a39e2455.jpg"; // <-- oyage photo eka danna
+import java from "../../assets/java.svg";
+import jsIcon from "../../assets/js-icon.svg";
+import reactIcon from "../../assets/react-icon.svg";
+import springIcon from "../../assets/Spring_Boot.svg";
+import mysqlIcon from "../../assets/mysql-icon.svg";
 import htmlIcon from "../../assets/html-icon.svg";
 import cssIcon from "../../assets/css-icon.svg";
-import jsIcon from "../../assets/js-icon.svg";
-// import nodeIcon from "../../assets/node-icon.svg";
-import reactIcon from "../../assets/react-icon.svg";
-import typescriptIcon from "../../assets/typescript-icon.svg";
-import vueIcon from "../../assets/vue-icon.svg";
-import boostrapIcon from "../../assets/bootstrap-icon.svg";
+import bootstrapIcon from "../../assets/bootstrap-icon.svg";
+import nodeIcon from "../../assets/node-icon.svg";
+import firebaseIcon from "../../assets/Firebase_Logo_(No_wordmark)_(2024-).svg";
 import ScrollAnimation from "react-animate-on-scroll";
 
 export function About() {
@@ -19,109 +17,101 @@ export function About() {
       <Container id="about">
         <div className="about-text">
           <ScrollAnimation animateIn="fadeInLeft">
-            <h2>About me</h2>
+            <h2>About Me</h2>
           </ScrollAnimation>
-          <ScrollAnimation animateIn="fadeInLeft" delay={0.1 * 1000}>
+
+          <ScrollAnimation animateIn="fadeInLeft" delay={100}>
             <p>
-              Hi there! I'm Vinayak, an enthusiastic Software Engineer skilled in web development, machine learning, and scalable software solutions
+              I’m a passionate and hardworking Software Engineering student at IJSE,
+              with hands-on experience in developing, testing, and maintaining
+              software applications. I love solving problems, learning new
+              technologies, and building scalable software that meets real user needs.
             </p>
           </ScrollAnimation>
-          <ScrollAnimation animateIn="fadeInLeft" delay={0.2 * 1000} style={{ marginTop: "2rem", marginBottom: "2rem" }}>
-            <p>
-              I build responsive web applications, fine-tune ML models, and implement robust backend services.
-            </p>
-          </ScrollAnimation>
-          <ScrollAnimation animateIn="fadeInLeft" delay={0.3 * 1000}>
-            <p>
-              I also work with CMS platforms like WordPress and Shopify to streamline content management.
-            </p>
-          </ScrollAnimation>
-          <ScrollAnimation animateIn="fadeInLeft" delay={400}>
+
+          <ScrollAnimation animateIn="fadeInLeft" delay={200}>
             <div className="education">
               <h3>Education:</h3>
-              <h4> Master of Computer Applications (MCA)</h4>
-              <p>Vellore Institute of Technology, Chennai | July 2023 - Present</p>
-              <p>8.09 CGPA</p>
-            </div>
-          </ScrollAnimation>
-          <ScrollAnimation animateIn="fadeInLeft" delay={550}>
-            <div className="experience">
-              <h3>Experience:</h3><h4>Software Developer</h4>
-              <p>KIWIS AND BROWNIES IT SOLUTIONS | October 2024 - Present</p>
-              <p>Bangalore, India</p>
+              <h4>Institute of Software Engineering (IJSE)</h4>
+              <p>Aug 2023 – Present</p>
+              <br/>
+              <ul>
+                <li>Enterprise software development with Java, Spring Boot, JavaFX</li>
+                <li>Dynamic UIs with React & Angular</li>
+                <li>Database integration with MySQL & PostgreSQL</li>
+                <li>Full-stack development using the MERN stack (MongoDB, Express.js, React, Node.js)</li>
+                <li>Version control & automation using Git, Docker, Maven</li>
+              </ul>
+
+
+              <h4>Kaluthara Vidyalaya</h4>
+              <p>2019 – 2022 (G.C.E. Advanced Level – Commerce Stream)</p>
             </div>
           </ScrollAnimation>
 
-          <ScrollAnimation animateIn="fadeInLeft" delay={0.4 * 1000}>
-            <h3>Here are my main skills:</h3>
+          <ScrollAnimation animateIn="fadeInLeft" delay={300}>
+            <h3>Main Skills</h3>
           </ScrollAnimation>
+
           <div className="hard-skills">
             <div className="hability">
-              <ScrollAnimation animateIn="fadeInUp" delay={0.10 * 1000}>
-                <img src={python} alt="python" />
+              <ScrollAnimation animateIn="fadeInUp" delay={100}>
+                <img src={java} alt="Java" />
               </ScrollAnimation>
             </div>
             <div className="hability">
-              <ScrollAnimation animateIn="fadeInUp" delay={0.11 * 1000}>
-                <img src={java} alt="java" />
+              <ScrollAnimation animateIn="fadeInUp" delay={110}>
+                <img src={springIcon} alt="Spring Boot" />
               </ScrollAnimation>
             </div>
             <div className="hability">
-              <ScrollAnimation animateIn="fadeInUp" delay={0.12 * 1000}>
+              <ScrollAnimation animateIn="fadeInUp" delay={120}>
                 <img src={jsIcon} alt="JavaScript" />
               </ScrollAnimation>
             </div>
             <div className="hability">
-              <ScrollAnimation animateIn="fadeInUp" delay={0.13 * 1000}>
+              <ScrollAnimation animateIn="fadeInUp" delay={130}>
                 <img src={reactIcon} alt="React" />
               </ScrollAnimation>
             </div>
             <div className="hability">
-              <ScrollAnimation animateIn="fadeInUp" delay={0.14 * 1000}>
-                <img src={typescriptIcon} alt="Typescript" />
+              <ScrollAnimation animateIn="fadeInUp" delay={140}>
+                <img src={htmlIcon} alt="HTML" />
               </ScrollAnimation>
             </div>
             <div className="hability">
-              <ScrollAnimation animateIn="fadeInUp" delay={0.15 * 1000}>
-                <img src={vueIcon} alt="Vue" />
+              <ScrollAnimation animateIn="fadeInUp" delay={150}>
+                <img src={cssIcon} alt="CSS" />
               </ScrollAnimation>
             </div>
             <div className="hability">
-              <ScrollAnimation animateIn="fadeInUp" delay={0.16 * 1000}>
-                <img src={wordpress} alt="Wordpress" />
+              <ScrollAnimation animateIn="fadeInUp" delay={160}>
+                <img src={bootstrapIcon} alt="Bootstrap" />
               </ScrollAnimation>
             </div>
             <div className="hability">
-              <ScrollAnimation animateIn="fadeInUp" delay={0.17 * 1000}>
-                <img src={shopify} alt="shopify" />
+              <ScrollAnimation animateIn="fadeInUp" delay={170}>
+                <img src={mysqlIcon} alt="MySQL" />
               </ScrollAnimation>
             </div>
             <div className="hability">
-              <ScrollAnimation animateIn="fadeInUp" delay={0.18 * 1000}>
-                <img src={htmlIcon} alt="Html" />
+              <ScrollAnimation animateIn="fadeInUp" delay={180}>
+                <img src={nodeIcon} alt="Node.js" />
               </ScrollAnimation>
             </div>
             <div className="hability">
-              <ScrollAnimation animateIn="fadeInUp" delay={0.19 * 1000}>
-                <img src={cssIcon} alt="Css" />
-              </ScrollAnimation>
-            </div>
-            <div className="hability">
-              <ScrollAnimation animateIn="fadeInUp" delay={0.20 * 1000}>
-                <img src={boostrapIcon} alt="bootstrap" />
+              <ScrollAnimation animateIn="fadeInUp" delay={190}>
+                <img src={firebaseIcon} alt="Firebase" />
               </ScrollAnimation>
             </div>
           </div>
         </div>
+
         <div className="about-image">
-          <ScrollAnimation animateIn="fadeInRight" delay={0.21 * 1000}>
-            <img
-                src={VinayakSingh}
-                alt="Vinayak Singh"
-                className="profile-img"
-            />
+          <ScrollAnimation animateIn="fadeInRight" delay={210}>
+            <img src={myPhoto} alt="My Profile" className="profile-img" />
           </ScrollAnimation>
         </div>
       </Container>
-  )
+  );
 }
